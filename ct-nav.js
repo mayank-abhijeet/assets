c16d55fda3f5de7426e6d6e2a69f78dd68f22d46
mayank-abhijeet <38348159+mayank-abhijeet@users.noreply.github.com>
2018-05-24 13:27:24 +0530

    updated mobileViewControl

--- ct-nav.js
+++ ct-nav.js
@@ -15,24 +15,81 @@ var heading_mobile = document.getElementById("heading-mobile");
 var home=document.querySelector(".home");
 var content=document.querySelector(".content");
 var for_you_overlay=document.getElementById("for_you_overlay");
+var bread=document.querySelector(".bread");
 
 var sticky = navigation.offsetTop;
 var hamSticky = ham.offsetTop;
 
+document.addEventListener("DOMContentLoaded", function (event) {
 
-home.addEventListener("mouseenter",function(event){
-    for_you_overlay.classList.remove("hidden");  
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        
+        ham.classList.remove("hideHam");
+        ham.classList.add("show");
+        heading_mobile.classList.remove("hide");
+        for (i = 0; i < list.length; i++) {
+            list[i].classList.add("hide");
+        }
+    } else {
+        
+        heading_mobile.classList.add("hide");
+        ham.classList.add("hideHam");
+        heading.classList.add("hide");
+
+    }
+});
+
+
+home.addEventListener("mouseover",function(event){
+    for_you_overlay.classList.remove("hidden"); 
+    if (window.pageYOffset-5 >= sticky){
+        for_you_overlay.classList.add("advertisement-scroll");
+        for_you_overlay.classList.remove("advertisement");
+    }else{
+        for_you_overlay.classList.remove("advertisement-scroll");
+        for_you_overlay.classList.add("advertisement");
+    }
+         
+    
+    console.log("over home 1"); 
 });
 
 for_you_overlay.addEventListener("mouseenter",function(event){
-    for_you_overlay.classList.remove("hidden");  
+    for_you_overlay.classList.remove("hidden"); 
+    if (window.pageYOffset-5 >= sticky){
+        for_you_overlay.classList.add("advertisement-scroll");
+        for_you_overlay.classList.remove("advertisement");
+    }else{
+        for_you_overlay.classList.remove("advertisement-scroll");
+        for_you_overlay.classList.add("advertisement");
+    }    
 });
 for_you_overlay.addEventListener("mouseleave",function(event){
-    for_you_overlay.classList.add("hidden");  
+    for_you_overlay.classList.add("hidden"); 
+    if (window.pageYOffset-5 >= sticky){
+        for_you_overlay.classList.add("advertisement-scroll");
+        for_you_overlay.classList.remove("advertisement");
+    }else{
+        for_you_overlay.classList.remove("advertisement-scroll");
+        for_you_overlay.classList.add("advertisement");
+    }        
+     
 });
 
 home.addEventListener("click",function(){
 for_you_overlay.classList.toggle("hidden");
+if (window.pageYOffset-5 >= sticky){
+    for_you_overlay.classList.add("advertisement-scroll");
+    for_you_overlay.classList.remove("advertisement");
+}else{
+    for_you_overlay.classList.remove("advertisement-scroll");
+    for_you_overlay.classList.add("advertisement");
+}    
+
+});
+
+bread.addEventListener("mouseover",function(){
+    console.log("over bread");
 });
 
 if(navigation.classList.contains("sticky")){
@@ -43,22 +100,7 @@ else{
     
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
 
-    if (window.matchMedia("(max-width: 768px)").matches) {
-        ham.classList.remove("hideHam");
-        ham.classList.add("show");
-        heading_mobile.classList.remove("hide");
-        for (i = 0; i < list.length; i++) {
-            list[i].classList.add("hide");
-        }
-    } else {
-        heading_mobile.classList.add("hide");
-        ham.classList.add("hideHam");
-        heading.classList.add("hide");
-
-    }
-});
 
 
 function scroll_function(){
@@ -98,6 +140,8 @@ function makeitstick() {
         ham.classList.remove("show");
         heading.classList.remove("show");
         heading.classList.add("hide");
+        for_you_overlay.classList.remove("advertisement-scroll");
+        for_you_overlay.classList.add("advertisement");
         for (i = 0; i < list.length; i++) {
             list[i].classList.remove("hide");
         }
@@ -131,11 +175,15 @@ function mobileViewControl() {
         ham.classList.add("show");
         heading_mobile.classList.remove("hide");
         heading.classList.remove("show");
+        ham.classList.remove("hideHam");        
         content.classList.remove("padding-for-content");    
         for (i = 0; i < list.length; i++) {
             list[i].classList.add("hide");
         }
+        
     } else {
         heading_mobile.classList.add("hide");
+        ham.classList.add("hideHam");
+        heading.classList.add("hide");
     }
 }
